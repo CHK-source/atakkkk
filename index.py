a4cdf5263a4eee293500b7fb6380b262ff59df8e
CHK-source <68204151+CHK-source@users.noreply.github.com>
2022-04-07 11:15:51 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,30 @@
+import os;
+os.system('python cpf.py')
+
+print("""
+ ,ggg,        gg                                            
+dP""Y8b       88                      8I                    
+Yb, `88       88                      8I                    
+ `"  88       88                      8I                    
+     88aaaaaaa88                      8I                    
+     88"""""""88    ,gggg,gg    ,gggg,8I   ,ggg,     ,g,    
+     88       88   dP"  "Y8I   dP"  "Y8I  i8" "8i   ,8'8,   
+     88       88  i8'    ,8I  i8'    ,8I  I8, ,8I  ,8'  Yb  
+     88       Y8,,d8,   ,d8b,,d8,   ,d8b, `YbadP' ,8'_   8) 
+     88       `Y8P"Y8888P"`Y8P"Y8888P"`Y8888P"Y888P' "YY8P8P
+                          by JhinStroKE.
+""")
+print("")
+print("")
+print("""
+Menu de Haquiagems KKKKKKK
+    """)
+comando = input('> ')
+
+if comando == '1':
+    system("echo Hello World")
+elif comando == '2':
+    os.system('python cpf.py')
+    input('pa eae')
+elif comando == '3':
+    print("aasasas")
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+title Start Hades
+python index.py
